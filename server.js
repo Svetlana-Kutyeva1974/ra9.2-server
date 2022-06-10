434fd0ff5bd9db1cd77cde003fa80f87a869d541
Svetlana Kutyova <kutyovas@yandex.ru>
2022-06-10 22:17:08 +0300

    serv

--- server.js
+++ server.js
@@ -7,20 +7,38 @@ const koaBody = require('koa-body');
 const app = new Koa();
 
 app.use(cors());
-app.use(koaBody({json: true}));
+//app.use(koaBody({json: true}));
 
-let posts = [];
+app.use(koaBody({
+   multipart: true,
+   json: true,
+   urlencoded: true
+}));
+// {id: '0', content: `hello`, created: Date.now()}
+//let posts = [{id: '0', content: `hello`, created: Date.now()}];
+//let nextId = 1;
+
 let nextId = 1;
+let posts = [
+    {id:nextId++ ,content: 'React - современная библиотека ', created : '2022-11-12'},
+    {id:nextId++, content: 'Исполизование возможностей библиотеки React', created : '2022-02-12'},
+    {id:nextId++, content: 'Bootstrap -современный css', created : '2016-12-12'},
+    {id:nextId++ ,content: 'React  4', created : '2022-11-12'},
+    {id:nextId++, content: 'Исполизование 5', created : '2022-02-12'},
+    {id:nextId++, content: 'Boot-6', created : '2018-12-10'},
+];
 
 const router = new Router();
 
 router.get('/posts', async (ctx, next) => {
-    ctx.response.body = posts;
+    ctx.response.body = JSON.stringify(posts);
+    console.log("ctx-----get", ctx.response.body);
 });
 
 router.post('/posts', async(ctx, next) => {
-    const {id, content} = ctx.request.body;
-
+    const {id, content, created} = ctx.request.body;
+    const data = JSON.parse(ctx.request.body);
+    console.log("ctx----------", ctx.response.body,'data\n', data);
     if (id !== 0) {
         posts = posts.map(o => o.id !== id ? o : {...o, content: content});
         ctx.response.status = 204;
@@ -32,10 +50,12 @@ router.post('/posts', async(ctx, next) => {
 });
 
 router.delete('/posts/:id', async(ctx, next) => {
-    const postId = Number(ctx.params.id);
+    console.log("id backend----------", ctx, nextId);
+    const postId = JSON.parse(ctx.params.id);
     const index = posts.findIndex(o => o.id === postId);
     if (index !== -1) {
         posts.splice(index, 1);
+        console.log("posts delete backend res!!!!", posts);
     }
     ctx.response.status = 204;
 });
@@ -44,4 +64,4 @@ app.use(router.routes()).use(router.allowedMethods());
 
 const port = process.env.PORT || 7777;
 const server = http.createServer(app.callback());
-server.listen(port, () => console.log('server started'));
+server.listen(port, () => console.log('server started----'));
